9dd29503598cb1df74ded645833d703cc5408f6b
cesar juarez <cesar.juarez@codeimmersives.com>
2021-03-15 07:25:57 -0700

    code

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function addMeToEnd (arr1){
+arr1.push ('Colin');
+}
+function addMeToStart (arr1){
+arr1.unshift ('Colin');
+}
+function changeLast (arr1, newValue){
+for (let i = 0; i < arr1.length; i++){
+arr1 [arr1.length -1] = newValue;
+}
+}
+function changeAllValuesTo (arr1, value){
+for (let i = 0; i < arr1.length; i++){
+  arr1 [i] = value;
+}
+}
+
+function oddOrEven (arr1){
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1 [i] % 2 !== 0){
+      arr1 [i] = 'odd';
+    } else {
+      arr1 [i] = 'even'
+    }
+  }
+}
 
 
-
-
+function changeNextThreeToValue (arr1, index, value){
+for (let i = index; i < arr1.length; i+2){
+arr1 [i] = value;
+}
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
